ad3c013a046c9c90cb1e5e490461c9a774f43f40
ksenia.u <ksenia.u@find-bride.com>
2021-02-28 15:18:29 +0300

    removed homework plan

--- src/search/index.jsx
+++ src/search/index.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import './styles.css';
 
 const Search = props => {
-  // 1. разметку, т.е. использовтаь input и застилизовать (border, width, height)
-  // 2. useState в todoList (search state)
-  // 3. props onChange (из useState) для Search component
   const onChangeHandler = ({ currentTarget }) => {
     props.onChange(currentTarget.value);
   };
