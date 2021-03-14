ddd8443cd1f96ee564c211ebedee104d250327a9
ksenia.u <ksenia.u@find-bride.com>
2021-03-14 19:22:35 +0300

    фикс верстки search input

--- src/modal/index.jsx
+++ src/modal/index.jsx
@@ -9,12 +9,16 @@ const root = document.getElementById('root');
 const Modal = props => {
   const modal = (
     <div className={cn("modal", { 'modal--opened': props.isOpened })}>
-      <div className="modal-close" onClick={props.onClose}><CloseIcon className="close-icon" /></div>
+      <div className="modal-close" onClick={props.onClose}>
+        <CloseIcon className="close-icon" />
+      </div>
       <div className="modal-container">
         <div>
             <h2>{props.title}</h2>
             <div>{props.children}</div>
-            <div className="modal-close-container" onClick={props.onClose}>Ok</div>
+            <div className="modal-close-container" onClick={props.onClose}>
+              Ok
+            </div>
         </div>
       </div>
     </div>
